d88415436b19918311f1fe63e12744558aa6c30d
amitkardwal <amitkardwal320@gmail.com>
2024-10-05 18:32:18 +0530

    Add .env file and update SearchBox component

--- mini-project-react/src/Searchbox.jsx
+++ mini-project-react/src/Searchbox.jsx
@@ -2,60 +2,54 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./SearchBox.css";
 import { useState } from 'react';
-import { colors } from '@mui/material';
 
-export default function SearchBox({updateInfo}) {
+export default function SearchBox({ updateInfo }) {
     let [city, setCity] = useState("");
-    let [error,seterror]=useState(false);
+    let [error, setError] = useState(false);
 
-    const API_URL ="https://api.openweathermap.org/data/2.5/weather"; 
-    const API_KEY = "a3d74ba5875400e4f2316c30f17d6eea";
+const API_URL="https://api.openweathermap.org/data/2.5/weather";
+const API_KEY="a3d74ba5875400e4f2316c30f17d6eea"
 
-    let getWeatherInfo=async()=>{
-      try{
-        let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-        let jsonResponse=await response.json();
-       
-       
-        let result={
-         city:city,
-         temp:jsonResponse.main.temp,
-         tempMin : jsonResponse.main.temp_min,
-         tempMax  : jsonResponse.main.temp_max,
-         humidity: jsonResponse.main.humidity,
-         feelsLike : jsonResponse.main.feels_like,
-         weather : jsonResponse.weather[0].description,
- 
-        };
-        console.log(result);
-        return result;
-      }catch(err){
-       throw err;
-      }
+
+    let getWeatherInfo = async () => {
+        try {
+            let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+            let jsonResponse = await response.json();
+
+            let result = {
+                city: city,
+                temp: jsonResponse.main.temp,
+                tempMin: jsonResponse.main.temp_min,
+                tempMax: jsonResponse.main.temp_max,
+                humidity: jsonResponse.main.humidity,
+                feelsLike: jsonResponse.main.feels_like,
+                weather: jsonResponse.weather[0].description,
+            };
+            console.log(result);
+            return result;
+        } catch (err) {
+            throw err;
+        }
     }
 
     let handleChange = (evt) => {
         setCity(evt.target.value);
     };
 
-    let handleSubmit = async(evt) => {
-   try{
-    evt.preventDefault();
-    console.log(city);
-    setCity(""); 
-  let newinfo= await getWeatherInfo();
-  updateInfo(newinfo);
-
-   }catch(err){
-    seterror(true);
-
-   }
-
+    let handleSubmit = async (evt) => {
+        try {
+            evt.preventDefault();
+            console.log(city);
+            setCity("");
+            let newInfo = await getWeatherInfo();
+            updateInfo(newInfo);
+        } catch (err) {
+            setError(true);
+        }
     };
 
     return (
         <div className='SearchBox'>
-           
             <form onSubmit={handleSubmit}>
                 <TextField 
                     id="city"
@@ -67,9 +61,8 @@ export default function SearchBox({updateInfo}) {
                 />
                 <br /><br />
                 <Button variant="contained" type="submit">Search</Button>
-                {error&& <p style={{color:"Red"}}>NO such place in our API</p>}
+                {error && <p style={{ color: "Red" }}>NO such place in our API</p>}
             </form>
         </div>
     );
 }
-
